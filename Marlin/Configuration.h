479758818114cb5dc5e630374e0ad0776a8dc8d0
Rémi LACHAUX <contact@remi-lachaux.fr>
2025-01-10 22:14:02 +0100

    Configuration for Creality CR-10 - extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -741,7 +741,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 138.081632653 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 140.198632 }
 
 /**
  * Default Max Feed Rate (mm/s)
